938417010b4e11f8ff276b1159b0f0c321494c62
Test <pat.lis@interia.pl>
2021-05-28 22:35:21 +0200

    Init

---
+++ yourapp/__init__.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.secret_key = 'parafia'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # tworzy punkt końcowy auth
+
+items = []
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="To pole nie może być puste!"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "pozycja o nazwie '{}' już istnieje".format(name)}
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'pozycja została usunięta'}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+if __name__ == '__main__':
+    app.run(debug=True)  # ważne do debugowania debug=True
---
+++ yourapp/user.py
@@ -0,0 +1,5 @@
+class User(object):
+    def _init_(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
